6f45e0f562e46bcda958ad4b9c4ca7bd0602c572
FlyingFish520 <2358443996@qq.com>
2017-11-05 23:25:56 +0800

    end

--- pages/cal/cal.js
+++ pages/cal/cal.js
@@ -25,7 +25,9 @@ Page({
     id19: "log",
     id20: "=",
     screenData: "0",
-    lastIsOperator: "false"
+    lastIsOperator: "false",
+    arr:[],
+    logs:[]
 
   },
 
@@ -85,34 +87,137 @@ Page({
     
   },
 
+  log: function(){
+    wx.navigateTo({
+      url: '../list/list',
+    })
+  },
+
   btnClick: function (event) {
     console.log(event.target.id);
     var id = event.target.id;
     var sd = this.data.screenData;
     var data;
 
-    //叠加运算符时的判断，运算符不重复，初始界面为0时不叠加
-    if(id==this.data.id4||id==this.data.id8||id==this.data.id12||id==this.data.id16){
-      if(this.data.lastIsOperator==true||this.data.screenData==0){
+    //退格
+    if(id == this.data.id1){
+      var data = this.data.screenData;
+      if(data == 0){
         return ;
       }
+      data = data.substring(0,data.length-1);
+      if(data==""||data=="-"){
+        data=0;
+      }
+      this.setData({screenData:data});
+      this.data.arr.pop();  //退格清掉数组
     }
 
-    //显示输入字符
-    if (sd == 0){
-      data = id;
+    //清屏操作
+    else if(id ==this.data.id2){
+      this.setData({screenData:"0"});
+      this.data.arr.length=0;
     }
+    
+    //正负转换
+    else if(id ==this.data.id3){
+      var data = this.data.screenData;
+      if(data==0){
+        return ;
+      }
+      var firstWord = data.substring(0,1);
+      if(firstWord == "-"){
+        data = data.substring(1,data.length);
+        this.data.arr.shift();
+      }
+      else{
+        data = "-"+data;
+        this.data.arr.unshift("-");
+      }
+      this.setData({screenData:data});
+    }
+    
+    //等号操作
+    else if(id == this.data.id20){
+      var data = this.data.screenData;
+      if(data==0){
+        return ;
+      }
+      var lastWord = data.substring(data.length-1,data.length);//等号前必须是数字
+      if (isNaN(lastWord)) {   //NaN 属性是代表非数字值的特殊值，该属性用于指示某个值不是数字。
+        return ;
+      }
+
+      var num = "";
+      var lastOperator;
+      var arr = this.data.arr; //储存输入字符数组
+      var optarr = [];  //储存运算式
+      for(var i in arr){
+        if(isNaN(arr[i])==false||arr[i]==this.data.id18||arr[i]==this.data.id3){
+          num += arr[i];
+        }
+        else{
+          lastOperator = arr[i];
+          optarr.push(num);
+          optarr.push(arr[i]);
+          num = "";
+        }
+      }
+      optarr.push(Number(num));
+      var result = Number(optarr[0])*1.0;
+      console.log(result+"(结果)");
+      for(var i=1;i<optarr.length;i++){
+        if(isNaN(optarr[i])){  //判断运算符及其操作
+          if(optarr[i]==this.data.id4){
+            result += Number(optarr[i+1]); 
+          }
+          else if (optarr[i] == this.data.id8){
+            result -= Number(optarr[i + 1]);
+          }
+          else if (optarr[i] == this.data.id12) {
+            result *= Number(optarr[i + 1]);
+          }
+          else if (optarr[i] == this.data.id16) {
+            result /= Number(optarr[i + 1]);
+          }
+        }
+      }
+
+      this.data.logs.push(data+"="+result);
+      wx.setStorageSync("callogs",this.data.logs);  //存日志
+
+      //算完清空数组
+      this.data.arr.length = 0;
+      this.data.arr.push(result);
+      this.setData({screenData:result+""});
+    } 
+
+    //显示屏
     else{
-      data = sd + id; //相加要注意前后顺序，这是字符的连接
-    }
-    this.setData({screenData:data});
+      //叠加运算符时的判断，运算符不重复，初始界面为0时不叠加
+      if (id == this.data.id4 || id == this.data.id8 || id == this.data.id12 || id == this.data.id16) {
+        if (this.data.lastIsOperator == true || this.data.screenData == 0) {
+          return;
+        }
+      }
 
-    //设置运算符不能连续添加
-    if (id == this.data.id4 || id == this.data.id8 || id == this.data.id12 || id == this.data.id16) {
-      this.setData({ lastIsOperator: true });
-    }
-    else {
-      this.setData({ lastIsOperator: false });
+      //显示输入字符
+      if (sd == 0) {
+        data = id;
+      }
+      else {
+        data = sd + id; //相加要注意前后顺序，这是字符的连接
+      }
+      this.setData({ screenData: data });
+      this.data.arr.push(id);
+
+      //设置运算符不能连续添加
+      if (id == this.data.id4 || id == this.data.id8 || id == this.data.id12 || id == this.data.id16) {
+        this.setData({ lastIsOperator: true });
+      }
+      else {
+        this.setData({ lastIsOperator: false });
+      }
     }
   }
 })
